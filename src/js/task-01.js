587b07665e3b17866f92330c50bfbf6d86bcde19
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2025-03-18 17:17:35 +0200

    one

---
+++ src/js/task-01.js
@@ -0,0 +1,15 @@
+const test = document.querySelector('ul')
+const item = document.querySelectorAll('.item')
+// console.log("Number of categories:",test.children.length)
+
+console.log(test)
+// console.log(item.children)
+// console.log(item.forEach(el => {
+//     console.log(el)
+// }))
+// test.children.forEach(el => {
+//     console.log(el)
+// });
+
+
+ 
