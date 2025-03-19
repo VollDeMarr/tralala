a8ab79d510ab388740e7c9585f4201e9de8203f8
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2025-03-19 19:23:07 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
+const btn = document.querySelector("button");
 
+form.addEventListener("submit", onSubmit);
+const res = {};
+function onSubmit(ev) {
+  ev.preventDefault();
+  const short = ev.target.elements;
+
+  if (short.email.value === "" || short.password.value === "") {
+    return alert("Попередження про те, що всі поля повинні бути заповнені.");
+  }
+  res.email = short.email.value;
+  res.password = short.password.value;
+
+  console.log(res);
+  form.reset();
+}
