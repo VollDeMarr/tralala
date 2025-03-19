7901ff8fa8bf615cc7f45032c72c4b91c06972e0
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2025-03-19 19:35:23 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+btn.addEventListener("click", onClick);
+
+function onClick() {
+  const res = getRandomHexColor();
+
+  body.style.backgroundColor = res;
+  span.textContent = res;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
